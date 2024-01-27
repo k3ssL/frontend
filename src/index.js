90020765676a0d4c72bc96b3f9e303e06da5f039
k3ssL <viqwer3@yandex.ru>
2024-01-27 23:19:54 +0500

    i scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './app/global.css';
+import './app/global.scss';
 import App from './app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
